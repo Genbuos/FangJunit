3ca1ab342567c9fe40e7578053b1f95124e7aa0f
genbuos <swiftypython18@gmail.com>
2024-08-26 18:06:59 -0400

    tests passed

--- src/main/java/twoSum/Solution.java
+++ src/main/java/twoSum/Solution.java
@@ -1,8 +1,18 @@
 package twoSum;
 
+import java.util.HashMap;
+
 public class Solution {
     public int[] twoSum(int[] nums, int target){
+        HashMap<Integer, Integer> numMap = new HashMap<>();
 
+        for(int i = 0; i < nums.length; i++){
+            int difference = target - nums[i];
+            if(numMap.containsKey(difference)){
+                return new int[] {numMap.get(difference), i};
+            }
+            numMap.put(nums[i], i);
+        }
         return null;
     }
 }
--- src/test/java/TwoSumSolution.java
+++ src/test/java/TwoSumSolution.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import twoSum.Solution;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TwoSumSolution {
@@ -6,16 +8,27 @@ public class TwoSumSolution {
     void testCase1(){
         int[] nums = {2,7,11,15};
         int target = 9;
+        Solution s = new Solution();
+       int[] expected = {0,1};
+        assertArrayEquals(expected, s.twoSum(nums, target) );
     }
     @Test
     void testCase2(){
         int[] nums = {3,2,4};
         int target = 6;
+        Solution s = new Solution();
+        int[] expected = {1,2};
+        assertArrayEquals(expected, s.twoSum(nums, target) );
+
     }
     @Test
     void testCase3(){
-        int[] nums = {3,3,4};
+        int[] nums = {3,5,4};
         int target = 7;
+        Solution s = new Solution();
+        int[] expected = {0,2};
+        assertArrayEquals(expected, s.twoSum(nums, target) );
+
     }
 
 
