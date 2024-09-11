cd042f14614bc4c7e29b01f991aed466efcd83e4
Genbuos <Swiftypython18@gmail.com>
2024-09-11 00:05:02 -0400

    Palindrome 1 complete

---
+++ src/main/java/strings/ValidPalindromeSolution.java
@@ -0,0 +1,25 @@
+package strings;
+
+public class ValidPalindromeSolution {
+    public boolean vps(String str){
+        int left = 0;
+        int right = str.length() - 1;
+        while(left < right){
+
+            while(left < right && !Character.isLetterOrDigit(str.charAt(left))){
+                left++;
+            }
+
+            while ((left < right && !Character.isLetterOrDigit(str.charAt(right)))){
+                right--;
+            }
+            if(Character.toLowerCase(str.charAt(left)) != Character.toLowerCase(str.charAt(right))){
+              return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
